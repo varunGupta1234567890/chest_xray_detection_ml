49687d9cb18bdfd71f63abe12c98f021176876a7
varunGupta1234567890 <gvarun4220@gmail.com>
2026-04-07 12:35:04 +0530

    UI added through streamlit

--- app.py
+++ app.py
@@ -1,88 +1,88 @@
 
-# from fastapi import FastAPI, File, UploadFile, Request
-# from fastapi.responses import HTMLResponse
-# from fastapi.templating import Jinja2Templates
-# from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
-# import uvicorn
-# import cv2
-# import numpy as np
-# import os
-# import pickle
-# import time
-# from tensorflow.keras.models import load_model
-
-
-# app = FastAPI()
+import uvicorn
+import cv2
+import numpy as np
+import os
+import pickle
+import time
+from tensorflow.keras.models import load_model
 
 
-# model = load_model('models/CNN_Covid19_Xray_Version.h5')
-# le = pickle.load(open("models/Label_encoder.pkl", 'rb'))
+app = FastAPI()
 
 
-# UPLOAD_FOLDER = "uploads"
-# os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
-# app.mount("/uploads", StaticFiles(directory=UPLOAD_FOLDER), name="uploads")
+model = load_model('models/CNN_Covid19_Xray_Version.h5')
+le = pickle.load(open("models/Label_encoder.pkl", 'rb'))
 
 
-# templates = Jinja2Templates(directory="templates")
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+app.mount("/uploads", StaticFiles(directory=UPLOAD_FOLDER), name="uploads")
 
 
-# def process_image(image_path):
-#     image = cv2.imread(image_path)
-
-#     if image is None:
-#         raise ValueError("Image not found")
-
-#     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#     image = cv2.resize(image, (150, 150))
-#     image = image / 255.0
-#     image = np.expand_dims(image, axis=0)
-
-#     predictions = model.predict(image)
-#     idx = np.argmax(predictions)
-
-#     label = le.inverse_transform([idx])[0]
-#     confidence = float(predictions[0][idx])
-
-#     return label, confidence
+templates = Jinja2Templates(directory="templates")
 
 
-# @app.get("/", response_class=HTMLResponse)
-# async def home(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
+def process_image(image_path):
+    image = cv2.imread(image_path)
+
+    if image is None:
+        raise ValueError("Image not found")
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (150, 150))
+    image = image / 255.0
+    image = np.expand_dims(image, axis=0)
+
+    predictions = model.predict(image)
+    idx = np.argmax(predictions)
+
+    label = le.inverse_transform([idx])[0]
+    confidence = float(predictions[0][idx])
+
+    return label, confidence
 
 
-# @app.post("/upload", response_class=HTMLResponse)
-# async def upload_file(request: Request, file: UploadFile = File(...)):
-#     try:
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/upload", response_class=HTMLResponse)
+async def upload_file(request: Request, file: UploadFile = File(...)):
+    try:
     
-#         filename = f"{int(time.time())}_{file.filename.replace(' ', '_')}"
-#         file_path = os.path.join(UPLOAD_FOLDER, filename)
+        filename = f"{int(time.time())}_{file.filename.replace(' ', '_')}"
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
 
         
-#         with open(file_path, "wb") as buffer:
-#             buffer.write(await file.read())
+        with open(file_path, "wb") as buffer:
+            buffer.write(await file.read())
 
         
-#         label, confidence = process_image(file_path)
+        label, confidence = process_image(file_path)
 
-#         return templates.TemplateResponse("result.html", {
-#             "request": request,
-#             "image_path": f"/uploads/{filename}",   # IMPORTANT
-#             "predicted_label": label,
-#             "confidence_score": round(confidence * 100, 2)
-#         })
+        return templates.TemplateResponse("result.html", {
+            "request": request,
+            "image_path": f"/uploads/{filename}",   # IMPORTANT
+            "predicted_label": label,
+            "confidence_score": round(confidence * 100, 2)
+        })
 
-#     except Exception as e:
-#         return HTMLResponse(f"<h2>Error: {str(e)}</h2>")
-
-
-# @app.get("/camera", response_class=HTMLResponse)
-# async def camera(request: Request):
-#     return templates.TemplateResponse("camera.html", {"request": request})
+    except Exception as e:
+        return HTMLResponse(f"<h2>Error: {str(e)}</h2>")
 
 
-# if __name__ == "__main__":
-#     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+@app.get("/camera", response_class=HTMLResponse)
+async def camera(request: Request):
+    return templates.TemplateResponse("camera.html", {"request": request})
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
